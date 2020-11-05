86abc6c3e67759782ff4be1c76ae7bff700571d9
yuxiangli8@126.com <yuxiangli8@126.com>
2020-11-06 01:02:07 +0800

    完整版，增，查

--- day3-save-query/session/record.go
+++ day3-save-query/session/record.go
@@ -13,9 +13,10 @@ func (s *Session) Insert(values ...interface{}) (int64, error) {
 		recordValues = append(recordValues, table.RecordValues(value))
 	}
 
-	//没加... 加了...之后相当于是个数组了，每个数组元素可能又是不一样的类型的值。比如我穿一个切片过去，那么接受方a[0]就是个切片，
-	//如果我没有加... 那么接受方a[0].b[0]才是切片。
-	s.caluse.Set(caluse.VALUES, recordValues...)
+	//没加... 会多一层层级
+	//加了...之后相当于是个数组了，每个数组元素可能又是不一样的类型的值。比如我穿一个切片过去，那么接受方a[0]就是个切片，
+	//如果我没有加... 我传的是个切片。本身就是个数组。所以数组加可变参数（本身是数组）所以就多了一层
+	s.caluse.Set(caluse.VALUES, recordValues)
 	sql, vars := s.caluse.Build(caluse.INSERT, caluse.VALUES)
 	result, err := s.Raw(sql, vars...).Exec()
 	if err != nil {
